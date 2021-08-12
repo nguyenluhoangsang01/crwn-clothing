e6b9fedbfb8289da5372360bbe3a42cce2f32d91
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2021-08-12 13:53:38 +0700

    add type=button to CustomButton sign in with google

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -60,7 +60,7 @@ const SignIn = () => {
 
         <div>
           <CustomButton type="submit">Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+          <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
             Sign in with Google
           </CustomButton>
         </div>
