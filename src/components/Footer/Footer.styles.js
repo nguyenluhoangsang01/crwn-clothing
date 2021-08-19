881d750f2592273aee2f95bb4509f7c69172bbfc
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2021-08-19 18:48:13 +0700

    change styles

--- src/components/Footer/Footer.styles.js
+++ src/components/Footer/Footer.styles.js
@@ -2,5 +2,5 @@ import styled from "styled-components";
 
 export const FooterWrapper = styled.div`
   text-align: center;
-  margin: 50px 0 20px;
+  margin: 60px 0 20px;
 `;
--- src/pages/CheckoutPage/CheckoutPage.styles.js
+++ src/pages/CheckoutPage/CheckoutPage.styles.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const CheckoutPageWrapper = styled.div`
   width: 55%;
-  min-height: 90vh;
   display: flex;
   flex-direction: column;
   align-items: center;
