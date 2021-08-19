d4afc7941908c554ba7b3a85dd501a1050e214f7
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2021-08-19 09:59:01 +0700

    added redux-logger in development env

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,15 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
 import { persistStore } from "redux-persist";
-
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+const middleware = [];
+
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
+
+const store = createStore(rootReducer, applyMiddleware(...middleware));
 
 const persistor = persistStore(store);
 
