b6b353625d377d121f93494efb2df58d96a23e59
Nguyen Lu Hoang Sang <hoangsang.nguyen2001a@gmail.com>
2021-08-25 10:59:24 +0700

    added redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
+import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 
-const middleware = [];
+const middleware = [thunk];
 
 if (process.env.NODE_ENV === "development") {
   middleware.push(logger);
